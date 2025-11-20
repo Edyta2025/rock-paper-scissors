7699ef439c7be986d3bcef28a385708944e2d4b3
Edyta <edziorka87@gmail.com>
2025-11-20 11:13:08 +0000

    add features

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-let playerOne = prompt("player one type your move");
-let playerTwo = prompt("player two type your move");
+const btn = document.querySelector("button");
+const palyerScoreTxt = document.querySelector(".playerScore");
+const computerScoreTxt = document.querySelector(".computerScore");
 
+let palyerScore = 0;
+let computerScore = 0;
+
+btn.addEventListener("click", handler);
+
+function handler () {
+  let playerOne = prompt("player one type your move");
+  let computerPlayer = null;
+
+// generate random number
+
+let randonNumber = Math.floor(Math.random() * 3) + 1;
+
+if (randonNumber === 1) {
+   computerPlayer = "scissors";
+} else if (randonNumber === 2) {
+   computerPlayer = "rock";
+} else {
+   computerPlayer = "paper";
+};
+
+
+// Game
 
 // if palyer one type scissors
 
 if (playerOne === "scissors") {
-    if (playerTwo === "rock"){
-        alert("player two won");
-    } else if (playerTwo === "paper") {
+    if (computerPlayer === "rock"){
+        computerScore += 1;
+        computerScoreTxt.innerHTML = computerScore;
+        alert("computer won");
+    } else if (computerPlayer === "paper") {
+        palyerScore += 1;
+        palyerScoreTxt.innerHTML = palyerScore;
         alert("player one won");
     } else {
         alert("draw");
@@ -17,9 +45,13 @@ if (playerOne === "scissors") {
 // if palyer one type rock
 
 if (playerOne === "rock") {
-    if (playerTwo === "paper"){
-        alert("player two won");
-    } else if (playerTwo === "scissors") {
+    if (computerPlayer === "paper"){
+        computerScore += 1;
+        computerScoreTxt.innerHTML = computerScore;
+        alert("computer won");
+    } else if (computerPlayer === "scissors") {
+        palyerScore += 1;
+        palyerScoreTxt.innerHTML = palyerScore;
         alert("player one won");
     } else {
         alert("draw");
@@ -29,13 +61,33 @@ if (playerOne === "rock") {
 // if palyer one type paper
 
 if (playerOne === "paper") {
-    if (playerTwo === "scissors"){
-     alert("player two won");
-    } else if (playerTwo === "rock") {
+    if (computerPlayer === "scissors"){
+        computerScore += 1;
+        computerScoreTxt.innerHTML = computerScore;
+        alert("computer won");
+    } else if (computerPlayer === "rock") {
+        palyerScore += 1;
+        palyerScoreTxt.innerHTML = palyerScore;
         alert("player one won");
     } else {
         alert("draw");
     }
+};
+
+// counter
+
+if (palyerScore > 1 || computerScore > 1){
+    palyerScore = 0;
+    computerScore = 0;
+    palyerScoreTxt.innerHTML = palyerScore;
+    computerScoreTxt.innerHTML = computerScore;
+   if (palyerScore > computerScore){
+    alert("Player won the game")
+   } else {
+    alert("Computer won the game")
+   };
+}
+
 };
 
 
